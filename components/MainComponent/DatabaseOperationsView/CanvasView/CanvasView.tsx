eb481a05b2b9aedb92381afcf184da2156903342
mapleleafjack <jack.musajo@madetech.com>
2022-11-07 22:46:46 +0000

    Readded three as its the only decent library

--- components/MainComponent/DatabaseOperationsView/CanvasView/CanvasView.tsx
+++ components/MainComponent/DatabaseOperationsView/CanvasView/CanvasView.tsx
@@ -1,16 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { DataThingy } from 'types';
+import { Canvas } from '@react-three/fiber'
 
 interface ComponentProps {
     data: DataThingy[];
 }
 
 const loadData = (data: DataThingy[]) => {
-    let pos = (data.length / 2) * -1.5;
+    let pos = (data.length) * -1;
 
     let element = data.map(elm => {
-
-        return elm
+        let dato = <mesh position={[pos, 0, 0]} scale={[3, 3, 3]}>
+            <boxBufferGeometry attach="geometry" />
+            <meshBasicMaterial attach="material" color="orange" opacity={1} transparent />
+        </mesh>
+        pos += 4
+        return dato
     })
     return element
 }
@@ -18,7 +23,12 @@ const loadData = (data: DataThingy[]) => {
 const CanvasView: FC<ComponentProps> = ({ data }) => {
     const loadedData = loadData(data)
 
-    return <>{JSON.stringify(loadedData)}</>
+    return <Canvas style={{ background: "black" }}>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+
+        {loadedData}
+    </Canvas>
 };
 
 export default CanvasView;
