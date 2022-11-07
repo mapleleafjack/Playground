ee0245dc84f85a33291e72aa97110eb54224b58e
mapleleafjack <jack.musajo@madetech.com>
2022-11-07 18:01:14 +0000

    Added spinner

--- components/MainComponent/DatabaseOperationsView/DatabaseOperationsView.tsx
+++ components/MainComponent/DatabaseOperationsView/DatabaseOperationsView.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Elevation } from '@blueprintjs/core';
+import { Button, Card, Elevation, Spinner } from '@blueprintjs/core';
 import axios from 'axios';
 import ElementCards from 'components/MainComponent/DatabaseOperationsView/ElementCards/ElementCards';
 import React, { FC, useEffect, useState } from 'react';
@@ -10,35 +10,43 @@ import s from './DatabaseOperatiosView.module.scss';
 
 const DatabaseOperationsView: FC = () => {
     const [data, setData] = useState<DataThingy[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         refreshData()
     }, [])
 
     const refreshData = async () => {
+        setLoading(true)
         try {
             let data = await getThingy("thingy");
             setData(data)
         } catch (err) {
             console.log(err);
         }
+        setLoading(false)
     };
 
 
     return (
         <div className={`${s.panelLayout}`}>
+
             <div className={`${s.buttonPanel}`}>
                 <DatabaseOperations refreshData={refreshData} />
             </div >
 
             <div className={`${s.panel}`}>
-                <div className={`${s.elementsPanel}`}>
-                    <ElementCards data={data} callback={refreshData} />
-                </div>
+                {loading ? <Spinner /> :
+                    <>
+                        <div className={`${s.elementsPanel}`}>
+                            <ElementCards data={data} callback={refreshData} />
+                        </div>
 
-                <div className={`${s.canvasPanel}`}>
-                    <CanvasView data={data} />
-                </div>
+                        <div className={`${s.canvasPanel}`}>
+                            <CanvasView data={data} />
+                        </div>
+                    </>
+                }
             </div>
 
         </div>
