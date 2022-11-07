393f8388002dee11b3bd5ed4cbdcd142ba9f3b16
mapleleafjack <jack.musajo@madetech.com>
2022-11-07 18:11:14 +0000

    three initial setup

--- components/MainComponent/DatabaseOperationsView/CanvasView/CanvasView.tsx
+++ components/MainComponent/DatabaseOperationsView/CanvasView/CanvasView.tsx
@@ -1,3 +1,4 @@
+import { Canvas } from '@react-three/fiber';
 import React, { FC } from 'react';
 import { DataThingy } from 'types';
 
@@ -7,9 +8,15 @@ interface ComponentProps {
 
 const CanvasView: FC<ComponentProps> = ({ data }) => {
     return (
-        <div>
-            {JSON.stringify(data)}
-        </div>
+        <Canvas>
+            <ambientLight intensity={0.1} />
+            <directionalLight color="red" position={[0, 0, 5]} />
+            <mesh>
+                <boxGeometry />
+                <meshStandardMaterial />
+            </mesh>
+        </Canvas>
+
     );
 };
 
