7da725118ebcbc3981a5d33d0413246c5962554f
mapleleafjack <jack.musajo@madetech.com>
2023-01-04 21:10:35 +0000

    Refactor: decopuled element from audio engine

--- components/MainComponent/AudioVisualiserView/AudioVisualiserView.tsx
+++ components/MainComponent/AudioVisualiserView/AudioVisualiserView.tsx
@@ -1,30 +1,11 @@
-import { NumeroContext } from 'lib/audioProvider';
 import React, { FC, useContext, useEffect, useState } from 'react';
 
-import s from './AudioVisualiserView.module.scss';
-
-const createElement = (numberOfEls: number) => {
-    var elements = [];
-
-    for (let i = 0; i < numberOfEls; i++) {
-        const hue = Math.round(360 / numberOfEls * i);
-        const rotate = (360 / numberOfEls) * i;
-
-        let style = {
-            "backgroundColor": 'hsl(' + hue + ', 40%, 60%)',
-            "transform": 'rotate(' + rotate + "deg" + ') scale(1)'
-        }
-        elements.push(<div key={"elm" + i} id={"elm" + i} style={style} className={`${s.el}`}></div>)
-    }
-
-    return elements
-}
+import { FlowerView, updateFlowerView } from './FlowerView/FlowerView';
 
 const AudioVisualiserView: FC = () => {
     const audioContext = new AudioContext();
     const analyser = audioContext.createAnalyser();
 
-    // Set up microphone input
     navigator.mediaDevices
         .getUserMedia({ audio: true })
         .then((stream) => {
@@ -32,28 +13,12 @@ const AudioVisualiserView: FC = () => {
             microphone.connect(analyser);
         });
 
-    const numero = useContext(NumeroContext);
-
     const idleAnimation = (fps: number) => {
-        let next_elm = 0;
         var drawAlt = function () {
-            // Get frequency data from the analyser
             const data = new Uint8Array(analyser.frequencyBinCount);
             analyser.getByteFrequencyData(data);
 
-            for (let i = 0; i < 100; i++) {
-                const rotate = (360 / 100) * i;
-
-                var el = document.getElementById("elm" + i);
-
-                // Calculate scale based on the amplitude of the current slice of the analyzer
-                const scale = data[i] / 100;
-
-                if (el) {
-                    el.style.transform = "rotate(" + rotate + "deg" + ") scale(" + scale + ")";
-                    // el.style.backgroundColor = "orange";
-                }
-            }
+            updateFlowerView(data, flowerShape)
 
             setTimeout(() => {
                 requestAnimationFrame(drawAlt);
@@ -61,14 +26,32 @@ const AudioVisualiserView: FC = () => {
         };
         requestAnimationFrame(drawAlt);
     };
+    const [flowerShape, setFlowerShape] = useState("flower");
+
+
 
     useEffect(() => {
-        numero && idleAnimation(numero);
-    }, []);
+        idleAnimation(60)
+    }, [flowerShape]);
+
+    const onchangeShape = (value: string) => {
+        setFlowerShape(value)
+    }
 
     return (
-        <div className={`${s.wrapper}`}>
-            <div className={`${s.mainwheel}`}>{createElement(100)}</div>
+        <div>
+            <FlowerView flowerShape={flowerShape} />
+            <label htmlFor="flower-shape-select">Choose flower shape:</label>
+            <select
+                id="flower-shape-select"
+                value={flowerShape}
+                onChange={(event) => {
+                    onchangeShape(event.target.value)
+                }}
+            >
+                <option value="line">Line</option>
+                <option value="flower">Flower</option>
+            </select>
         </div>
     );
 };
---
+++ components/MainComponent/AudioVisualiserView/FlowerView/FlowerView.tsx
@@ -0,0 +1,72 @@
+import { FC, useState } from 'react';
+import s from './FlowerView.module.scss';
+
+const createElement = (numberOfEls: number, flowerShape: string) => {
+    var elements = [];
+
+    for (let i = 0; i < numberOfEls; i++) {
+        const hue = Math.round(360 / numberOfEls * i);
+
+        let style;
+
+        if (flowerShape == "flower") {
+            const rotate = (360 / numberOfEls) * i;
+
+            style = {
+                "backgroundColor": 'hsl(' + hue + ', 40%, 60%)',
+                "transform": 'rotate(' + rotate + "deg" + ') scale(1)'
+            }
+        } else {
+            const translateX = (i / numberOfEls) * 100;
+
+            style = {
+                "backgroundColor": "hsl(" + hue + ", 40%, 60%)",
+                "transform": "translateX(" + translateX + "%) translateY(100%) scale(1)"
+            };
+        }
+
+        elements.push(<div key={"elm" + i} id={"elm" + i} style={style} className={`${s.el}`}></div>)
+    }
+
+    return elements
+}
+
+export const redrawFlowerView = (data: Uint8Array, flowerShape: string) => {
+    // Recreate all the UI elements using the updated flowerShape and data values
+    const elements = createElement(100, flowerShape);
+
+    // Clear the existing elements from the DOM
+    document.querySelectorAll(`.${s.el}`).forEach(el => el.remove());
+}
+
+export const updateFlowerView = (data: Uint8Array, flowerShape: string) => {
+    for (let i = 0; i < 100; i++) {
+        const rotate = (360 / 100) * i;
+        const translateX = (100) * i;
+
+        var el = document.getElementById("elm" + i);
+
+        const scale = data[i] / 100;
+
+        if (el) {
+            if (flowerShape == "flower") {
+                el.style.transform = "rotate(" + rotate + "deg" + ") scale(" + scale + ")";
+            } else {
+                el.style.transform = "translateX(" + translateX + "%) translateY(100%) scale(" + scale + ")";
+            }
+        }
+    }
+}
+
+
+type FlowerViewProps = {
+    flowerShape: string;
+}
+
+export const FlowerView: FC<FlowerViewProps> = ({ flowerShape }) => {
+    return (
+        <div className={`${s.wrapper}`}>
+            <div className={`${s.mainwheel}`}>{createElement(100, flowerShape)}</div>
+        </div>
+    );
+}
