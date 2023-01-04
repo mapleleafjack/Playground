491306af0c518b42d4973482e2699029b0896cab
mapleleafjack <jack.musajo@madetech.com>
2023-01-04 18:51:35 +0000

    Added microphone input (yay ChatGPT!)

--- components/MainComponent/AudioVisualiserView/AudioVisualiserView.tsx
+++ components/MainComponent/AudioVisualiserView/AudioVisualiserView.tsx
@@ -21,50 +21,56 @@ const createElement = (numberOfEls: number) => {
 }
 
 const AudioVisualiserView: FC = () => {
+    const audioContext = new AudioContext();
+    const analyser = audioContext.createAnalyser();
 
-    const numero = useContext(NumeroContext)
+    // Set up microphone input
+    navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+            const microphone = audioContext.createMediaStreamSource(stream);
+            microphone.connect(analyser);
+        });
+
+    const numero = useContext(NumeroContext);
 
     const idleAnimation = (fps: number) => {
-
-        let next_elm = 0
+        let next_elm = 0;
         var drawAlt = function () {
+            // Get frequency data from the analyser
+            const data = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(data);
+
             for (let i = 0; i < 100; i++) {
-                const hue = Math.round(360 / 100 * i);
                 const rotate = (360 / 100) * i;
 
-                var el = document.getElementById('elm' + i);
+                var el = document.getElementById("elm" + i);
+
+                // Calculate scale based on the amplitude of the current slice of the analyzer
+                const scale = data[i] / 100;
 
                 if (el) {
-                    if (next_elm == i) {
-                        el.style.transform = 'rotate(' + rotate + "deg" + ') scale(1.3)';
-                        el.style.backgroundColor = 'orange';
-                    } else {
-                        el.style.transform = 'rotate(' + rotate + "deg" + ') scale(1)';
-                        el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)'
-                    }
+                    el.style.transform = "rotate(" + rotate + "deg" + ") scale(" + scale + ")";
+                    el.style.backgroundColor = "orange";
                 }
-            }
-
-            next_elm++;
-
-            if (next_elm == 100) {
-                next_elm = 0
             }
 
             setTimeout(() => {
                 requestAnimationFrame(drawAlt);
             }, 1000 / fps);
-        }
+        };
         requestAnimationFrame(drawAlt);
-    }
+    };
 
     useEffect(() => {
-        numero && idleAnimation(numero)
-    }, [])
+        numero && idleAnimation(numero);
+    }, []);
 
-    return <div className={`${s.wrapper}`}>
-        <div className={`${s.mainwheel}`}>{createElement(100)}</div>
-    </div>
+    return (
+        <div className={`${s.wrapper}`}>
+            <div className={`${s.mainwheel}`}>{createElement(100)}</div>
+        </div>
+    );
 };
 
 export default AudioVisualiserView;
